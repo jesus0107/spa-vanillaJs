c904efa0675ace757da91e721076ab406c7051ea
Jesus Cruz <bep010795@gmail.com>
2020-04-11 12:24:18 -0500

    Add styles

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,19 +1,40 @@
-const Character = () => {
+/**
+ * para renderizar la data de nuestros personaje importaremos el hash que contendra el id
+ * importaremos la data que hace la logica de la api
+ * 
+ * cambiamos nuestra funcion por async await
+ * cremoas una constante para almacenar el hash a la que denominaremos id
+ * crearemos una contante llamad character y le pasaremos por parametro el id que recibiremos en la otra contante
+ * 
+ * en esta ocacion no vamos a iterar sobre mas personajes, pues el id nos devuelve el personaje con el que trabajaremos, unicamente accederemos a alas propiedades que necesitamos 
+ * 
+ * hay propiedades en las que tendremos que acceder por mas de una propiedad
+ */
+
+
+import getHash from "../utils/getHash";
+import getData from "../utils/getData";
+
+const Character = async () => {
+
+    const id = await getHash();
+    const character = await getData(id);
+
     const view = /*html*/
         `
             <div class="Characters-inner">
                 <article class="Characters-card">
-                    <img src="image" alt="nombre_persona">
-                    <h2>Nombre de personaje</h2>
+                    <img src="${character.image}" alt="${character.name}">
+                    <h2>${character.name}</h2>
                 </article>
 
                 <article class="Characters-card">
-                    <h3>Episodes:</h3>
-                    <h3>Status:</h3>
-                    <h3>Species:</h3>
-                    <h3>Gender:</h3>
-                    <h3>Origin:</h3>
-                    <h3>Last Location:</h3>
+                    <h3>Episodes: <span>${character.episode.length}</span></h3>
+                    <h3>Status: <span>${character.status}</span></h3>
+                    <h3>Species: <span>${character.species}</span></h3>
+                    <h3>Gender: <span>${character.gender}</span></h3>
+                    <h3>Origin: <span>${character.origin.name}</span></h3>
+                    <h3>Last Location: <span>${character.location.name}</span></h3>
                 </article>
             </div>
         `
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,26 @@
  * retornaremos view
  * 
  * exportaremos este archivo para identificar si este archivo puede reutilizado en otros archvos js
- * importaremos la funcion que contiene el llamado a la api*/
+ * importaremos la funcion que contiene el llamado a la api
+ * 
+ * creamos una constante que almacenra la funcion que obtiene la data. le pasamos el await para esperar todo este llamado
+ * 
+ * como esta seccion es nuestro home no pasaremos un id sino que haremos el llamado a todos los personajes
+ * 
+ * crearemos una logica dentro de nuestro dv padre en el que llaamremos a character que contiene el llamado a la api. como analizamos con anterioridad la api sabemos que tenemos que acceder a results que es donde se encuentran las propiedades que necesitmos mostrar
+ * 
+ *  
+ * despues a results le aplicamos el metodo map para poder iterar sobre todos lo elementos, aqui pasamos por parametro  el personaje que retornaremos que contiene la data a iterar y despues generamos un template que sera el que iteraremos por cada personaje
+ * 
+ * map nos va a retornar un nuevo arreglo con la estructura que le pasaremos que en este caso sera el template pero con los valores que vamos a iterar
+ * 
+ * dentro del template accederemos ala s variables de cada persona haciendolo de forma dinamica
+ * 
+ * metodo join('') une todos los elementos de un array formando una cadena y separandolos con aquel argumentos que definmos o bien que pasamoss por parametro
+ * 
+ * generaremos un bloque que genera un arreglo que debemos unir para esto agregamos un join y le decimos que no utilice ninguna forma de separarlo
+ * con esto retornamos un bloque de html que sea mostrado en nuestra aplicacion 
+ * */
 
 import getData from '../utils/getData';
 
